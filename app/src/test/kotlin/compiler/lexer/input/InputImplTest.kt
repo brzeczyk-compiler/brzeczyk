d174e1b54d73f22b412637b8c044eb6ec346be73
Bartek Wacławik <bartek.waclawik@student.uj.edu.pl>
2022-10-14 21:46:10 +0200

    Rename and add some unit tests

--- app/src/test/kotlin/compiler/lexer/input/InputImplTest.kt
+++ app/src/test/kotlin/compiler/lexer/input/InputImplTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class InputImplTest {
-    @Test fun readNextCharacters() {
+    @Test fun `test gives correct characters`() {
         val text = "A\na ?ż"
         val input: Input = InputImpl(StringReader(text))
 
@@ -21,7 +21,50 @@ class InputImplTest {
         assertEquals('ż', input.next())
     }
 
-    @Test fun indicateEndOfText() {
+    @Test fun `test supports polish diacritics`() {
+        val text = "zażółć gęślą jaźń\nZAŻÓŁĆ GĘŚLĄ JAŹŃ"
+        val input: Input = InputImpl(StringReader(text))
+
+        assertEquals('z', input.next())
+        assertEquals('a', input.next())
+        assertEquals('ż', input.next())
+        assertEquals('ó', input.next())
+        assertEquals('ł', input.next())
+        assertEquals('ć', input.next())
+        assertEquals(' ', input.next())
+        assertEquals('g', input.next())
+        assertEquals('ę', input.next())
+        assertEquals('ś', input.next())
+        assertEquals('l', input.next())
+        assertEquals('ą', input.next())
+        assertEquals(' ', input.next())
+        assertEquals('j', input.next())
+        assertEquals('a', input.next())
+        assertEquals('ź', input.next())
+        assertEquals('ń', input.next())
+
+        assertEquals('\n', input.next())
+
+        assertEquals('Z', input.next())
+        assertEquals('A', input.next())
+        assertEquals('Ż', input.next())
+        assertEquals('Ó', input.next())
+        assertEquals('Ł', input.next())
+        assertEquals('Ć', input.next())
+        assertEquals(' ', input.next())
+        assertEquals('G', input.next())
+        assertEquals('Ę', input.next())
+        assertEquals('Ś', input.next())
+        assertEquals('L', input.next())
+        assertEquals('Ą', input.next())
+        assertEquals(' ', input.next())
+        assertEquals('J', input.next())
+        assertEquals('A', input.next())
+        assertEquals('Ź', input.next())
+        assertEquals('Ń', input.next())
+    }
+
+    @Test fun `test indicates end of text`() {
         val text = "xyz"
         val input: Input = InputImpl(StringReader(text))
 
@@ -34,7 +77,14 @@ class InputImplTest {
         assertFalse(input.hasNext())
     }
 
-    @Test fun getLocation() {
+    @Test fun `test indicates end of empty text`() {
+        val text = ""
+        val input: Input = InputImpl(StringReader(text))
+
+        assertFalse(input.hasNext())
+    }
+
+    @Test fun `test gives correct location`() {
         val text = "abc\nxy"
         val input: Input = InputImpl(StringReader(text))
 
@@ -53,7 +103,60 @@ class InputImplTest {
         assertEquals(Location(2, 3), input.getLocation())
     }
 
-    @Test fun illegalRewind() {
+    @Test fun `test gives correct location for empty text`() {
+        val text = ""
+        val input: Input = InputImpl(StringReader(text))
+
+        assertEquals(Location(1, 1), input.getLocation())
+    }
+
+    @Test fun `test gives correct location with many lines`() {
+        val text = "a\nb\ncc\nd\n\nf\ng\nh\ni\nj\n"
+        val input: Input = InputImpl(StringReader(text))
+
+        assertEquals(Location(1, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(1, 2), input.getLocation())
+        input.next()
+        assertEquals(Location(2, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(2, 2), input.getLocation())
+        input.next()
+        assertEquals(Location(3, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(3, 2), input.getLocation())
+        input.next()
+        assertEquals(Location(3, 3), input.getLocation())
+        input.next()
+        assertEquals(Location(4, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(4, 2), input.getLocation())
+        input.next()
+        assertEquals(Location(5, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(6, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(6, 2), input.getLocation())
+        input.next()
+        assertEquals(Location(7, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(7, 2), input.getLocation())
+        input.next()
+        assertEquals(Location(8, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(8, 2), input.getLocation())
+        input.next()
+        assertEquals(Location(9, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(9, 2), input.getLocation())
+        input.next()
+        assertEquals(Location(10, 1), input.getLocation())
+        input.next()
+        assertEquals(Location(10, 2), input.getLocation())
+        input.next()
+    }
+
+    @Test fun `test fails on illegal rewind`() {
         val text = "abcde"
         val input: Input = InputImpl(StringReader(text))
 
@@ -63,7 +166,20 @@ class InputImplTest {
         assertFails { input.rewind(3) }
     }
 
-    @Test fun readNextCharactersAfterRewind() {
+    @Test fun `test fails on illegal rewind after flush`() {
+        val text = "abcdefgh"
+        val input: Input = InputImpl(StringReader(text))
+
+        input.next()
+        input.next()
+        input.next()
+        input.flush()
+        input.next()
+
+        assertFails { input.rewind(2) }
+    }
+
+    @Test fun `test gives correct characters after rewind`() {
         val text = "Aą\nxy"
         val input: Input = InputImpl(StringReader(text))
 
@@ -79,7 +195,7 @@ class InputImplTest {
         assertEquals('y', input.next())
     }
 
-    @Test fun indicateEndOfTextAfterRewind() {
+    @Test fun `test indicates end of text after rewind`() {
         val text = "xyzt"
         val input: Input = InputImpl(StringReader(text))
 
@@ -97,7 +213,7 @@ class InputImplTest {
         assertFalse(input.hasNext())
     }
 
-    @Test fun getLocationAfterRewind() {
+    @Test fun `test gives correct location after rewind`() {
         val text = "abc\nxy"
         val input: Input = InputImpl(StringReader(text))
 
@@ -119,7 +235,7 @@ class InputImplTest {
         assertEquals(Location(2, 3), input.getLocation())
     }
 
-    @Test fun readNextCharactersAfterFlush() {
+    @Test fun `test gives correct characters after flush`() {
         val text = "Aąbxy"
         val input: Input = InputImpl(StringReader(text))
 
@@ -132,7 +248,7 @@ class InputImplTest {
         assertEquals('y', input.next())
     }
 
-    @Test fun indicateEndOfTextAfterFlush() {
+    @Test fun `test indicates end of text after flush`() {
         val text = "xyzt"
         val input: Input = InputImpl(StringReader(text))
 
@@ -147,7 +263,7 @@ class InputImplTest {
         assertFalse(input.hasNext())
     }
 
-    @Test fun getLocationAfterFlush() {
+    @Test fun `test gives correct location after flush`() {
         val text = "abc\nxy"
         val input: Input = InputImpl(StringReader(text))
 

e7d057bfcf6beb1bef013aa45343fa314b06d050
Bartek Wacławik <bartek.waclawik@student.uj.edu.pl>
2022-10-14 02:35:24 +0200

    Implement InputImpl

--- app/src/main/kotlin/compiler/lexer/input/InputImpl.kt
+++ app/src/main/kotlin/compiler/lexer/input/InputImpl.kt
@@ -2,29 +2,66 @@ package compiler.lexer.input
 
 import compiler.lexer.Location
 import java.io.Reader
+import java.util.Stack
 
-class InputImpl(source: Reader) : Input {
+class InputImpl(private val source: Reader) : Input {
 
-    override fun getLocation(): Location {
-        // TODO
-        return Location(0, 0)
-    }
+    private val characterBuffer = ArrayDeque<Char>()
+    private var pos: Int = 0
 
-    override fun rewind(count: Int) {
-        // TODO
-    }
+    private var row: Int = 1
+    private var col: Int = 1
+    private val lineStack = Stack<Int>()
 
-    override fun flush() {
-        // TODO
-    }
-
-    override fun hasNext(): Boolean {
-        // TODO
+    private fun ensureNextCharIsAvailable(): Boolean {
+        if (pos < characterBuffer.size)
+            return true
+        val nextChar = source.read()
+        if (nextChar == -1)
+            return false
+        characterBuffer.addLast(nextChar.toChar())
         return true
     }
 
+    override fun getLocation(): Location {
+        return Location(row, col)
+    }
+
+    override fun rewind(count: Int) {
+        if (count > pos)
+            throw IndexOutOfBoundsException()
+        pos -= count
+
+        var countLeft = count
+        while (col <= countLeft) {
+            countLeft -= col
+            row--
+            col = lineStack.pop()
+        }
+        col -= countLeft
+    }
+
+    override fun flush() {
+        characterBuffer.subList(0, pos).clear()
+        pos = 0
+        lineStack.clear()
+    }
+
+    override fun hasNext(): Boolean {
+        return ensureNextCharIsAvailable()
+    }
+
     override fun next(): Char {
-        // TODO
-        return Char.MIN_VALUE
+        if (!ensureNextCharIsAvailable())
+            throw NoSuchElementException()
+
+        val nextChar = characterBuffer[pos++]
+        if (nextChar == '\n') {
+            lineStack.push(col)
+            row++
+            col = 1
+        } else
+            col++
+        return nextChar
     }
 }

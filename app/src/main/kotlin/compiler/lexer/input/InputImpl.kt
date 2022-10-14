7a2f884432f72f643841574ab9e6267799c0035f
Bartek Wacławik <bartek.waclawik@student.uj.edu.pl>
2022-10-14 19:48:30 +0200

    Improve code readability

--- app/src/main/kotlin/compiler/lexer/input/InputImpl.kt
+++ app/src/main/kotlin/compiler/lexer/input/InputImpl.kt
@@ -7,14 +7,14 @@ import java.util.Stack
 class InputImpl(private val source: Reader) : Input {
 
     private val characterBuffer = ArrayDeque<Char>()
-    private var pos: Int = 0
+    private var position: Int = 0
 
     private var row: Int = 1
-    private var col: Int = 1
-    private val lineStack = Stack<Int>()
+    private var column: Int = 1
+    private val lineLengthStack = Stack<Int>()
 
     private fun ensureNextCharIsAvailable(): Boolean {
-        if (pos < characterBuffer.size)
+        if (position <= characterBuffer.lastIndex)
             return true
         val nextChar = source.read()
         if (nextChar == -1)
@@ -24,27 +24,27 @@ class InputImpl(private val source: Reader) : Input {
     }
 
     override fun getLocation(): Location {
-        return Location(row, col)
+        return Location(row, column)
     }
 
     override fun rewind(count: Int) {
-        if (count > pos)
+        if (count > position)
             throw IndexOutOfBoundsException()
-        pos -= count
+        position -= count
 
-        var countLeft = count
-        while (col <= countLeft) {
-            countLeft -= col
+        var charsLeft = count
+        while (column <= charsLeft) {
+            charsLeft -= column
             row--
-            col = lineStack.pop()
+            column = lineLengthStack.pop()
         }
-        col -= countLeft
+        column -= charsLeft
     }
 
     override fun flush() {
-        characterBuffer.subList(0, pos).clear()
-        pos = 0
-        lineStack.clear()
+        characterBuffer.subList(0, position).clear()
+        position = 0
+        lineLengthStack.clear()
     }
 
     override fun hasNext(): Boolean {
@@ -55,13 +55,14 @@ class InputImpl(private val source: Reader) : Input {
         if (!ensureNextCharIsAvailable())
             throw NoSuchElementException()
 
-        val nextChar = characterBuffer[pos++]
+        val nextChar = characterBuffer[position++]
         if (nextChar == '\n') {
-            lineStack.push(col)
+            lineLengthStack.push(column)
             row++
-            col = 1
-        } else
-            col++
+            column = 1
+        } else {
+            column++
+        }
         return nextChar
     }
 }

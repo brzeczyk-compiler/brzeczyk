bd1dcd04703a3a79a185e64484f20930be271f0e
Grzegorz Gawryał <grzegorz.gawryal@student.uj.edu.pl>
2022-10-19 12:03:11 +0200

    Rename getState to getStartState

--- app/src/main/kotlin/compiler/parser/grammar/AutomatonGrammar.kt
+++ app/src/main/kotlin/compiler/parser/grammar/AutomatonGrammar.kt
@@ -2,7 +2,7 @@ package compiler.parser.grammar
 import compiler.common.dfa.state_dfa.TransparentDfa
 
 class AutomatonGrammar<S : Comparable<S>> {
-    fun getState(): S {
+    fun getStartState(): S {
         return TODO()
     }
     fun getProductions(): Map<S, TransparentDfa<S, Production<S>>> {

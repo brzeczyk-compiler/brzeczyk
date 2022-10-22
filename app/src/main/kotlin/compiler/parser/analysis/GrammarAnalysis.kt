c32d18400e5b121d72c6a3556b30038af2cd070b
Ignacy Buczek <ignacy.buczek@onet.pl>
2022-10-22 16:57:39 +0200

    Implement nullable

--- app/src/main/kotlin/compiler/parser/analysis/GrammarAnalysis.kt
+++ app/src/main/kotlin/compiler/parser/analysis/GrammarAnalysis.kt
@@ -1,10 +1,41 @@
 package compiler.parser.analysis
 
+import compiler.common.dfa.state_dfa.Dfa
+import compiler.common.dfa.state_dfa.DfaState
 import compiler.parser.grammar.AutomatonGrammar
+import compiler.parser.grammar.Production
 
 class GrammarAnalysis<S : Comparable<S>> {
     fun computeNullable(grammar: AutomatonGrammar<S>): Set<S> {
-        return TODO()
+        val nullable: MutableSet<S> = HashSet()
+        val stateQueue: ArrayDeque<DfaState<S, Production<S>>> = ArrayDeque()
+        val conditionalSets: MutableMap<S, MutableSet<DfaState<S, Production<S>>>> = HashMap()
+
+        grammar.productions.forEach { (_, dfa) -> stateQueue.addAll(dfa.getAcceptingStates()) }
+        fun analyzePredecessors(state: DfaState<S, Production<S>>, dfa: Dfa<S, Production<S>>) {
+            val predecessorsMap = dfa.getPredecessors(state)
+            for ((edge, predecessors) in predecessorsMap) {
+                if (edge in nullable) {
+                    stateQueue.addAll(predecessors)
+                } else {
+                    conditionalSets.getOrPut(edge) { HashSet() }.addAll(predecessors)
+                }
+            }
+        }
+
+        while (stateQueue.size > 0) {
+            val state = stateQueue.removeFirst()
+            grammar.productions.forEach { (_, dfa) -> analyzePredecessors(state, dfa) }
+            // check if this is a startState for some production
+            for ((leftSideSymbol, rightSideDfa) in grammar.productions) {
+                if (state == rightSideDfa.startState) {
+                    nullable.add(leftSideSymbol)
+                    conditionalSets[leftSideSymbol]?.let { stateQueue.addAll(it) }
+                    conditionalSets[leftSideSymbol]?.let { it.clear() }
+                }
+            }
+        }
+        return nullable
     }
 
     fun computeFirst(grammar: AutomatonGrammar<S>, nullable: Set<S>): Map<S, Set<S>> {

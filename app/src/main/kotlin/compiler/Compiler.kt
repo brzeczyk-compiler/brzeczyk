ac0fc98d22d8ab0f4c0f964cbfbcc6aa5f4d6d4d
Ignacy Buczek <ignacy.buczek@onet.pl>
2022-11-01 15:31:25 +0100

    Add basic Compiler
    It assumes parser is not implemented yet.

--- app/src/main/kotlin/compiler/Compiler.kt
+++ app/src/main/kotlin/compiler/Compiler.kt
@@ -1,10 +1,26 @@
 package compiler
 
 import compiler.common.diagnostics.Diagnostics
+import compiler.lexer.Lexer
+import compiler.lexer.input.InputImpl
+import compiler.lexer.lexer_grammar.TokenType
+import compiler.lexer.lexer_grammar.Tokens
+import compiler.parser.ParseTree
+// import compiler.parser.Parser
+// import compiler.parser.grammar.ParserGrammar
+import compiler.parser.grammar.Symbol
 import java.io.Reader
 
 class Compiler(val diagnostics: Diagnostics) {
     fun process(input: Reader) {
-        TODO("Not yet implemented")
+        val lexer = Lexer<TokenType>(Tokens().getTokens(), diagnostics)
+        val tokenSequence = lexer.process(InputImpl(input))
+        val leaves: Sequence<ParseTree<Symbol>> = tokenSequence
+            .map { ParseTree.Leaf(it.start, it.end, Symbol.Terminal(it.category)) }
+
+        // consume the elements so that Lexer actually tries to evaluate
+        leaves.forEach { println(it) }
+
+        // val parseTree = Parser<Symbol>(ParserGrammar.getGrammar(), diagnostics).process(leaves)
     }
 }

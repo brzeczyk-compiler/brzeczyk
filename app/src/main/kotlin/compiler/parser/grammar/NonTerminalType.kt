a78562e801433995e538d62ce5bb098e1cc64587
Bartek Wacławik <bartek.waclawik@student.uj.edu.pl>
2022-11-04 19:28:30 +0100

    Add non-terminal symbols

--- app/src/main/kotlin/compiler/parser/grammar/NonTerminalType.kt
+++ app/src/main/kotlin/compiler/parser/grammar/NonTerminalType.kt
@@ -1,5 +1,19 @@
 package compiler.parser.grammar
 
 enum class NonTerminalType {
-    // TODO: define non terminal symbols
+    // root of parse tree
+    PROGRAM,
+    // types, literals
+    TYPE, CONST,
+    // argument lists
+    DEF_ARGS, DEF_ARG, DEF_ARG_WITH_DEFAULT,
+    CALL_ARGS, CALL_ARG_NAMED,
+    // variable/function declaration
+    VARIABLE_DECL, FUNCTION_DEF,
+    // expression categories
+    EXPR, EXPR2, EXPR4, EXPR8, EXPR16, EXPR32, EXPR64, EXPR128, EXPR256, EXPR512, EXPR1024, EXPR2048,
+    // statements
+    STATEMENT, NON_BRACE_STATEMENT, ATOMIC_STATEMENT,
+    // multiple statements
+    MAYBE_BLOCK, MANY_STATEMENTS
 }

6161d360d79bf49f38ce00b9714bf6a7a5ae4e6a
woocash2 <lukasgniecki@gmail.com>
2022-11-10 08:56:55 +0100

    Name resolution errors

--- app/src/main/kotlin/compiler/semantic_analysis/NameResolver.kt
+++ app/src/main/kotlin/compiler/semantic_analysis/NameResolver.kt
@@ -4,8 +4,16 @@ import compiler.ast.NamedNode
 import compiler.ast.Program
 import compiler.common.diagnostics.Diagnostics
 import compiler.common.semantic_analysis.ReferenceMap
+import java.lang.Exception
 
 object NameResolver {
+
+    sealed class NameResolutionError {
+        class AssignmentToUndefinedVariable(message: String) : Exception(message)
+        class UseOfUndefinedVariable(message: String): Exception(message)
+        class UseOfUndefinedFunction(message: String): Exception(message)
+    }
+
     fun calculateNameResolution(ast: Program, diagnostics: Diagnostics): ReferenceMap<Any, NamedNode> {
         return TODO()
     }

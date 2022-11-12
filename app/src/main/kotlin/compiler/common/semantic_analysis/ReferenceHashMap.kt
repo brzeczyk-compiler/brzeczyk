8fca1b4e305a237b8758a4e7ad79789790e85a06
Ignacy Buczek <ignacy.buczek@onet.pl>
2022-11-12 15:04:52 +0100

    Make ReferenceHashMap properly comparable

--- app/src/main/kotlin/compiler/common/semantic_analysis/ReferenceHashMap.kt
+++ app/src/main/kotlin/compiler/common/semantic_analysis/ReferenceHashMap.kt
@@ -4,4 +4,16 @@ import java.util.IdentityHashMap
 
 interface ReferenceMap<K, V> : Map<K, V>
 interface MutableReferenceMap<K, V> : MutableMap<K, V>, ReferenceMap<K, V>
-class ReferenceHashMap<K, V> : IdentityHashMap<K, V>(), MutableReferenceMap<K, V>
+class ReferenceHashMap<K, V> : IdentityHashMap<K, V>(), MutableReferenceMap<K, V> {
+    override fun equals(other: Any?): Boolean {
+        if (other is ReferenceHashMap<*, *>) {
+            if (this.size != other.size) return false
+            this.keys.forEach {
+                if (!other.containsKey(it)) return false
+                if (!this[it]!!.equals(other[it])) return false
+            }
+            return true
+        }
+        return false
+    }
+}

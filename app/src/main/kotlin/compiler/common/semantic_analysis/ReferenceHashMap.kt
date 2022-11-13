616ae8ae4acf0356ef59e5652df2a1dcba2ea631
Tomasz Buczyński <tomasz.buczynski@student.uj.edu.pl>
2022-11-13 03:12:34 +0100

    Add tests for the type checker

--- app/src/main/kotlin/compiler/common/semantic_analysis/ReferenceHashMap.kt
+++ app/src/main/kotlin/compiler/common/semantic_analysis/ReferenceHashMap.kt
@@ -3,5 +3,36 @@ package compiler.common.semantic_analysis
 import java.util.IdentityHashMap
 
 interface ReferenceMap<K, V> : Map<K, V>
+
 interface MutableReferenceMap<K, V> : MutableMap<K, V>, ReferenceMap<K, V>
-class ReferenceHashMap<K, V> : IdentityHashMap<K, V>(), MutableReferenceMap<K, V>
+
+class ReferenceHashMap<K, V> : IdentityHashMap<K, V>(), MutableReferenceMap<K, V> {
+    override fun equals(other: Any?): Boolean {
+        if (other is ReferenceHashMap<*, *>) {
+            if (this.size != other.size) return false
+            this.keys.forEach {
+                if (!other.containsKey(it)) return false
+                if (!this[it]!!.equals(other[it])) return false
+            }
+            return true
+        }
+        return false
+    }
+}
+
+class ReferenceEntry<K, V>(val key: K, var value: V) {
+    override fun equals(other: Any?): Boolean = other is ReferenceEntry<*, *> && key === other.key && value == other.value
+
+    override fun hashCode() = Pair(System.identityHashCode(key), value).hashCode()
+
+    override fun toString() = "$key to $value"
+}
+
+val <K, V> ReferenceMap<K, V>.referenceEntries get() = entries.map { ReferenceEntry(it.key, it.value) }.toSet()
+
+fun <K, V> referenceMapOf(vararg pairs: Pair<K, V>): ReferenceMap<K, V> {
+    val map = ReferenceHashMap<K, V>()
+    for ((key, value) in pairs)
+        map[key] = value
+    return map
+}

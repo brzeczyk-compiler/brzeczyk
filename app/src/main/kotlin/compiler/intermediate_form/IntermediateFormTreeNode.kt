04907127777b000512de3282238d5077e0c114b3
Grzegorz Gawryał <grzegorz.gawryal@student.uj.edu.pl>
2022-11-23 19:25:53 +0100

    Restructurise IFTNode

--- app/src/main/kotlin/compiler/intermediate_form/IntermediateFormTreeNode.kt
+++ app/src/main/kotlin/compiler/intermediate_form/IntermediateFormTreeNode.kt
@@ -1,82 +1,40 @@
 package compiler.intermediate_form
 
 sealed class IntermediateFormTreeNode {
-    data class MemoryRead(val register: Register, val address: Addressing) : IntermediateFormTreeNode() // mov reg, [addr]
-    data class MemoryWrite(val address: MemoryAddress, val register: Register) : IntermediateFormTreeNode() // mov [addr], reg
-    data class MemoryWriteConst(val address: MemoryAddress, val value: Long) : IntermediateFormTreeNode()
+    data class MemoryRead(val address: Addressing) : IntermediateFormTreeNode()
+    data class RegisterRead(val register: Register) : IntermediateFormTreeNode()
+    data class Const(val value: Long) : IntermediateFormTreeNode()
 
-    // no logical and, or, iff and xor, since they have a short circuit semantic
-    data class LogicalNegation(val resultRegister: Register) : IntermediateFormTreeNode()
+    data class MemoryWrite(val address: MemoryAddress, val node: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class RegisterWrite(val register: Register, val node: IntermediateFormTreeNode) : IntermediateFormTreeNode()
 
-    // arithmetic operations work on 32 bit registers
-    data class Add(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode() // add reg1, reg2
-    data class AddConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode() // add reg, 42
-    data class AddMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode() // add reg, [addr]
+    // no logical and, or, since they have a short circuit semantic
+    data class LogicalNegation(val node: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class LogicalIff(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class LogicalXor(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
 
-    data class Subtract(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class SubtractConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class SubtractMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
+    data class Add(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class Subtract(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class Multiply(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class Divide(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class Modulo(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
 
-    data class Multiply(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class MultiplyConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class MultiplyMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
+    data class BitAnd(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class BitOr(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class BitXor(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class BitShiftLeft(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class BitShiftRight(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class BitNegation(val node: IntermediateFormTreeNode) : IntermediateFormTreeNode()
 
-    data class Divide(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class DivideConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class DivideMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
+    data class Equals(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class NotEquals(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class LessThan(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class LessThanOrEquals(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class GreaterThan(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    data class GreaterThanOrEquals(val left: IntermediateFormTreeNode, val right: IntermediateFormTreeNode) : IntermediateFormTreeNode()
 
-    data class Modulo(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class ModuloConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class ModuloMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    // bitwise operations also work on 32 bit registers
-    data class BitAnd(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class BitAndConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class BitAndMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    data class BitOr(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class BitOrConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class BitOrMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    data class BitXor(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class BitXorConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class BitXorMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    data class BitShiftLeft(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class BitShiftLeftConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-
-    data class BitShiftRight(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class BitShiftRightConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-
-    data class BitNegation(val resultRegister: Register) : IntermediateFormTreeNode()
-
-    data class Equals(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode() // reg1 = (1 if reg1 == reg2, 0 otherwise)
-    data class EqualsConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class EqualsMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    data class NotEquals(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class NotEqualsConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class NotEqualsMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    data class LessThan(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class LessThanConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class LessThanMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    data class LessThanOrEquals(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class LessThanOrEqualsConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class LessThanOrEqualsMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    data class GreaterThan(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class GreaterThanConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class GreaterThanMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    data class GreaterThanOrEquals(val resultRegister: Register, val secondRegister: Register) : IntermediateFormTreeNode()
-    data class GreaterThanOrEqualsConst(val resultRegister: Register, val value: Int) : IntermediateFormTreeNode()
-    data class GreaterThanOrEqualsMem(val resultRegister: Register, val address: Addressing) : IntermediateFormTreeNode()
-
-    // stack operations, work on 64 bit registers
-    data class StackPush(val register: Register) : IntermediateFormTreeNode()
-    data class StackPop(val resultRegister: Register) : IntermediateFormTreeNode()
+    data class StackPush(val Node: IntermediateFormTreeNode) : IntermediateFormTreeNode()
+    class StackPop : IntermediateFormTreeNode()
 
     class NoOp : IntermediateFormTreeNode() // For testing
 }

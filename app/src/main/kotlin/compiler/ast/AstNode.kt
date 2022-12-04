a6e8f987d99ca9bb9dbb706df046a460d4570b64
JHodor <jedrzej.hodor@gmail.com>
2022-12-04 12:43:15 +0100

    Add a printer to AstNode interface

--- app/src/main/kotlin/compiler/ast/AstNode.kt
+++ app/src/main/kotlin/compiler/ast/AstNode.kt
@@ -4,4 +4,81 @@ import compiler.lexer.LocationRange
 
 sealed interface AstNode {
     val location: LocationRange?
+
+    fun printSimple(): String {
+        val stringBuilder = StringBuilder()
+
+        stringBuilder.append(
+                when (this) {
+                    is Program.Global -> "<<global definition>>"
+                    is Statement -> "<<statement>>"
+
+                    is Expression -> when (this) {
+                        is Expression.BinaryOperation -> "${this.leftOperand.printSimple()} ${this.kind} ${this.rightOperand.printSimple()}"
+                        is Expression.BooleanLiteral -> this.toString()
+                        is Expression.Conditional -> "${this.condition.printSimple()} ? ${this.resultWhenTrue.printSimple()} : ${this.resultWhenFalse.printSimple()}"
+                        is Expression.FunctionCall -> "${this.name}(${this.arguments.joinToString { it.printSimple() }})"
+                        is Expression.NumberLiteral -> this.toString()
+                        is Expression.UnaryOperation -> "${this.kind} ${this.operand}"
+                        is Expression.UnitLiteral -> "nic"
+                        is Expression.Variable -> this.name
+                    }
+
+                    is Variable -> "${this.kind} ${this.name}: ${this.type} = ${this.value}"
+                    is Function -> "czynność ${this.name}(${this.parameters.joinToString { it.printSimple() }}) -> ${this.returnType}"
+                    is Function.Parameter -> "${this.name}: ${this.type}${if (this.defaultValue != null) " = ${this.defaultValue.printSimple()}" else ""}"
+                    is Expression.FunctionCall.Argument -> "${if (this.name != null) "${this.name} = " else ""}${this.value.printSimple()}"
+
+                }
+        )
+
+        return stringBuilder.toString()
+    }
+
+    fun print(): String {
+        val stringBuilder = StringBuilder()
+
+        if (this.location != null) {
+            stringBuilder
+                    .append("at location ")
+                    .append("from (${this.location!!.start.row}, ${this.location!!.start.column}) ")
+                    .append("to (${this.location!!.end.row}, ${this.location!!.end.column})")
+        } else {
+            stringBuilder.append("at virtual location")
+        }
+
+        stringBuilder.append(" :: ")
+
+        stringBuilder.append(
+                when (this) {
+                    is Program.Global -> when (this) {
+                        is Program.Global.FunctionDefinition -> "definition of ${this.function.printSimple()}"
+                        is Program.Global.VariableDefinition -> "definition of ${this.variable.printSimple()}"
+                    }
+
+                    is Expression -> this.printSimple()
+
+                    is Statement -> when (this) {
+                        is Statement.Assignment -> "assignment ${this.variableName} = ${this.value.printSimple()}"
+                        is Statement.Block -> "{ ... }"
+                        is Statement.Conditional -> "jeśli - zaś gdy - wpp block with the condition (${this.condition.printSimple()})"
+                        is Statement.Evaluation -> "evaluation of ${this.expression.printSimple()}"
+                        is Statement.FunctionDefinition -> "definition of ${this.function.printSimple()}"
+                        is Statement.FunctionReturn -> "zwróć ${this.value}"
+                        is Statement.Loop -> "dopóki (${this.condition}) { ... }"
+                        is Statement.LoopBreak -> "przerwij"
+                        is Statement.LoopContinuation -> "pomiń"
+                        is Statement.VariableDefinition -> "definition of ${this.variable.printSimple()}"
+                    }
+
+                    is Variable -> "variable ${this.printSimple()}"
+                    is Function -> "function ${this.printSimple()}"
+                    is Function.Parameter -> "function parameter ${this.printSimple()}"
+                    is Expression.FunctionCall.Argument -> "function argument ${this.printSimple()}"
+
+                }
+        )
+
+        return stringBuilder.toString()
+    }
 }
--- app/src/main/kotlin/compiler/ast/Type.kt
+++ app/src/main/kotlin/compiler/ast/Type.kt
@@ -4,4 +4,10 @@ sealed class Type {
     object Unit : Type()
     object Boolean : Type()
     object Number : Type()
+
+    override fun toString(): String = when(this) {
+        Boolean -> "Czy"
+        Number -> "Liczba"
+        Unit -> "Nic"
+    }
 }

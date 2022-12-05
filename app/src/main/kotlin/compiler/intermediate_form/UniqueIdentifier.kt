2a8d9bd64a694ef37128b6f436e63e54b5b043da
Ignacy Buczek <ignacy.buczek@onet.pl>
2022-12-05 19:46:15 +0100

    Make UniqueIdentifierFactory more robust

--- app/src/main/kotlin/compiler/intermediate_form/UniqueIdentifier.kt
+++ app/src/main/kotlin/compiler/intermediate_form/UniqueIdentifier.kt
@@ -1,35 +1,70 @@
 package compiler.intermediate_form
 
-class UnknownCharacter(message: String) : RuntimeException(message)
+class IllegalCharacter(message: String) : RuntimeException(message)
+class InconsistentFunctionNamingConvention(message: String) : RuntimeException(message)
 
 class UniqueIdentifierFactory() {
-    private val knownConversionsToPlainASCII = mapOf(
-        'ą' to 'a',
-        'ć' to 'c',
-        'ę' to 'e',
-        'ł' to 'l',
-        'ó' to 'o',
-        'ś' to 's',
-        'ź' to 'z',
-        'ż' to 'z'
-    )
+    companion object {
+        // it might be handy to change these values with ease
+        public val functionPrefix = "fun"
+        public val levelSeparator = '$'
+        public val polishSignSymbol = '#' // added after polish signs are converted to allowed characters
+        public val charactersAllowedByNasm = listOf('a'..'z', 'A'..'Z', '0'..'9').flatten() +
+            listOf('_', '$', '#', '@', '~', '.', '?')
+        public val knownConversionsToAllowedCharacters = mapOf(
+            'ą' to 'a',
+            'ć' to 'c',
+            'ę' to 'e',
+            'ł' to 'l',
+            'ó' to 'o',
+            'ś' to 's',
+            'ź' to 'z',
+            'ż' to 'z',
+            'Ą' to 'A',
+            'Ć' to 'C',
+            'Ę' to 'E',
+            'Ł' to 'L',
+            'Ó' to 'O',
+            'Ś' to 'S',
+            'Ź' to 'Z',
+            'Ż' to 'Z'
+        )
+        public val forbiddenLabels = listOf("globals")
+    }
 
     private val knownIdentifiers: MutableSet<String> = mutableSetOf()
     fun build(prefix: String?, curr: String): UniqueIdentifier {
-        fun convertToPlainASCII(character: Char): Char {
-            if (character.code in 0..127) return character
-            if (character in knownConversionsToPlainASCII) return knownConversionsToPlainASCII[character]!!
-            throw UnknownCharacter("Unknown character: $character. Only polish signs are allowed outside of the plain ASCII characters.")
+        fun convertToPlainASCII(character: Char): List<Char> {
+            if (character in charactersAllowedByNasm) return listOf(character)
+            if (character in knownConversionsToAllowedCharacters) return listOf(
+                knownConversionsToAllowedCharacters[character]!!,
+                polishSignSymbol // to make mangled names more readable
+            )
+            throw IllegalCharacter(
+                """
+                Illegal character: $character.
+                Characters allowed by NASM for labels are: ${charactersAllowedByNasm.joinToString()}.
+            """
+            )
         }
-        val identifierBuilder = StringBuilder()
-        prefix?.let {
-            identifierBuilder.append(it)
-            identifierBuilder.append('|')
-        }
-        curr.forEach { identifierBuilder.append(convertToPlainASCII(it)) }
+        val identifierBuilder = StringBuilder().append(prefix ?: functionPrefix).append(levelSeparator)
+        curr.forEach { convertToPlainASCII(it).forEach { character -> identifierBuilder.append(character) } }
         var identifier = identifierBuilder.toString()
-        while (identifier in knownIdentifiers) identifier += "_"
-        knownIdentifiers.add(identifier)
+        // at the time of the addition of this class to the codebase, this should not happen ($ and # are not allowed in function names)
+        // this is a sanity check for if we ever relax the regex for function identifier and allow any of the special symbols used here
+        if (!knownIdentifiers.add(identifier))
+            throw InconsistentFunctionNamingConvention(
+                """
+            Two identical function labels were generated.
+            This means that the function naming convention is not consistent across all levels of compilation.
+            Are you allowing $levelSeparator or $polishSignSymbol in function names somewhere earlier on?
+            """
+            )
+
+        // should not happen with current convention
+        // sanity check in case we ever change the constants for this factory
+        if (identifier in forbiddenLabels) throw InconsistentFunctionNamingConvention("Name $identifier is forbidden.")
+
         return UniqueIdentifier(identifier)
     }
 }

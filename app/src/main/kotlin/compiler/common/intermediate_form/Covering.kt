79691f7b5a091d67ef206841c46cefae3585415a
Ignacy Buczek <ignacy.buczek@onet.pl>
2022-12-09 19:14:23 +0100

    Remove unnecessary detail from interface

--- app/src/main/kotlin/compiler/common/intermediate_form/Covering.kt
+++ app/src/main/kotlin/compiler/common/intermediate_form/Covering.kt
@@ -1,11 +1,9 @@
 package compiler.common.intermediate_form
 
 import compiler.intermediate_form.Instruction
-import compiler.intermediate_form.InstructionSet
 import compiler.intermediate_form.IntermediateFormTreeNode
 
 interface Covering {
-    val instructionSet: InstructionSet
     fun coverUnconditional(iftNode: IntermediateFormTreeNode): List<Instruction>
     fun coverConditional(iftNode: IntermediateFormTreeNode, targetLabel: String, invert: Boolean): List<Instruction>
 }
